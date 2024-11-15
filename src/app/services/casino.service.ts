f06ad603eaa43e6523cc5f01257254343218f129
Antonio González Luque <luquelectronico13@gmail.com>
2024-11-16 00:45:07 +0100

    "Added private access modifier to dictionary property in CasinoService"

--- src/app/services/casino.service.ts
+++ src/app/services/casino.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class CasinoService {
 
   private puntuacionTotal: number = 0;
-  dictionary: { [key: string]: string } = {
+  private dictionary: { [key: string]: string } = {
     'a': '1', 'b': '2', 'c': '3', 'd': '4', 'e': '5', 'f': '6', 'g': '7', 'h': '8', 'i': '9',
     'j': 'A', 'k': 'B', 'l': 'C', 'm': 'D', 'n': 'E', 'o': 'F', 'p': 'G', 'q': 'H', 'r': 'I',
     's': 'J', 't': 'K', 'u': 'L', 'v': 'M', 'w': 'N', 'x': 'O', 'y': 'P', 'z': 'Q',
