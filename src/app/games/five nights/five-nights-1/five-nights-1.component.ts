bd421f9b330d6a9f9b10ecf47035b153f5b0aade
Antonio G. Luque <luquelectronico13@gmail.com>
2024-11-16 01:19:20 +0100

    Componente creado para 5nights

---
+++ src/app/games/five nights/five-nights-1/five-nights-1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-five-nights-1',
+  standalone: true,
+  imports: [],
+  templateUrl: './five-nights-1.component.html',
+  styleUrl: './five-nights-1.component.css'
+})
+export class FiveNights1Component {
+
+}
